aa1fffae507e3a71cb3d0dd5895d703e3444ef31
Tunga Kantarcı <44529911+tungakantarci@users.noreply.github.com>
2025-08-10 16:27:35 +0200

    Add files via upload

--- exercise bootstrap block/exercise.m
+++ exercise bootstrap block/exercise.m
@@ -31,7 +31,7 @@ phi = 0.7;
 varepsilon = random('Normal',0,1,[N_obs_pop,1]);
 
 % 4.4. Simulate AR(1) time‑series data
-data_pop = filter(1,[1 -phi],varepsilon);
+data_pop = filter(1,[1-phi],varepsilon);
 
 %% 5. Plot AR(1) time series
 figure;
@@ -40,51 +40,51 @@ xlabel('t');
 ylabel('X_t');
 title('Fig. 1. First 1000 Points of the AR(1) Series');
 
-%% 6. Generate sample data
+%% 6. Draw samples from the population
 
 % 6.1. Set the sample size
 N_obs_sample = 5000;
 
-% 6.2. Randomly select a starting index for sampling
-start_idx = random('Discrete Uniform',N_obs_pop-N_obs_sample+1,[1,1]); % Ensures sample fits within bounds of data_pop
-
-% 6.3. Draw one contiguous sample from the population
-data_sample = data_pop(start_idx:start_idx+N_obs_sample-1); % Draw a block of consecutive data points from the population
-
-% 6.4. Compute optimal block length
-block_length = ceil(N_obs_sample^(1/5)); % Rule-of-thumb for block bootstrap
-
-% 6.5. Compute how many full blocks can be formed
-N_blocks = floor(N_obs_sample/block_length); % Maximum number of full blocks
-
-% 6.6. Trim sample to fit an exact number of full blocks
-trimmed_sample = data_sample(1:N_blocks*block_length);  % Ensures reshape works cleanly
-
-% 6.7. Reshape trimmed sample into non-overlapping blocks
-blocks = reshape(trimmed_sample,block_length,N_blocks)';
-
-%% 7. Draw (bootsrap) samples from the sample 
-
-% 7.1. Preallocate vector to store (bootsrap) sample means
-means_data_samples_boot = NaN(N_sim,1);
-
-% 7.2. Draw samples of k blocks from the original sample and compute the sample mean each time
-for i = 1:N_sim
-    data_samples_boot = datasample(blocks,N_blocks,1,'Replace',true);
-    data_samples_boot_flat = data_samples_boot(:);
-    means_data_samples_boot(i) = mean(data_samples_boot_flat);
-end
-
-%% 8. Draw samples from the population
-
-% 8.1. Preallocate vector to store sample means
+% 6.2. Preallocate vector to store sample means
 means_data_samples_pop = NaN(N_sim,1);
 
-% 8.2. Draw samples from the population and compute the sample mean each time
+% 6.3. Draw samples from the population and compute the sample mean each time
 for i = 1:N_sim
     start_idx = random('Discrete Uniform',N_obs_pop-N_obs_sample+1,[1,1]); 
     data_samples_pop = data_pop(start_idx:start_idx+N_obs_sample-1);
     means_data_samples_pop(i) = mean(data_samples_pop);
+end
+
+%% 7. Generate sample data
+
+% 7.1. Randomly select a starting index for sampling
+start_idx = random('Discrete Uniform',N_obs_pop-N_obs_sample+1,[1,1]); % Ensures sample fits within bounds of data_pop
+
+% 7.2. Draw one contiguous sample from the population
+data_sample = data_pop(start_idx:start_idx+N_obs_sample-1); % Draw a block of consecutive data points from the population
+
+% 7.3. Compute optimal block length
+block_length = ceil(N_obs_sample^(1/5)); % Rule-of-thumb for block bootstrap
+
+% 7.4. Compute how many full blocks can be formed
+N_blocks = floor(N_obs_sample/block_length); % Maximum number of full blocks
+
+% 7.5. Trim sample to fit an exact number of full blocks
+trimmed_sample = data_sample(1:N_blocks*block_length);  % Ensures reshape works cleanly
+
+% 7.6. Reshape trimmed sample into non-overlapping blocks
+blocks = reshape(trimmed_sample,block_length,N_blocks)';
+
+%% 8. Draw (bootsrap) samples from the original sample 
+
+% 8.1. Preallocate vector to store (bootsrap) sample means
+means_data_samples_boot = NaN(N_sim,1);
+
+% 8.2. Draw samples of k blocks from the original sample and compute the sample mean each time
+for i = 1:N_sim
+    data_samples_boot = datasample(blocks,N_blocks,1,'Replace',true);
+    data_samples_boot_flat = data_samples_boot(:);
+    means_data_samples_boot(i) = mean(data_samples_boot_flat);
 end
 
 %% 9. Plot the estimated PDFs
