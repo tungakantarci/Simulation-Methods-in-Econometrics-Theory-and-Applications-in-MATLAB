cdf9cfee7b47ae305de2952e7b51d560fda182a5
Tunga Kantarcı <44529911+tungakantarci@users.noreply.github.com>
2025-09-21 03:25:02 +0200

    Add files via upload

--- exercise assumption no measuremet error/exercise.m
+++ exercise assumption no measuremet error/exercise.m
@@ -24,53 +24,46 @@ B_true = 0.5;
 X = random("Uniform",-1,1,[N_obs 1]);
 
 % 3.6. Level of measurement error in terms of the SD of random noise
-mesurement_error_level = 0:0.1:1;
+measurement_error_level = 0:0.1:1;
 
 %% 4. Nested for loops for simulation and measurement error level
 
 % 4.1. Preallocate matrix to store OLS coefficient estimates
 B_hat = NaN(N_sim,1);
 
-% 4.4. Preallocate matrix to store estimates across mes. err. levels
-B_hat_error_level = NaN(N_sim,1,length(mesurement_error_level));
+% 4.2. Preallocate matrix to store estimates across mea. err. levels
+B_hat_measurement_error_level = NaN(N_sim, ...
+    1,length(measurement_error_level));
 
-% 4.2. Preallocate matrix to store estimation bias
-B_hat_bias = NaN(N_sim,1);
-
-% 4.3. Preall. matrix to store estimation bias across mes. err. levels
-B_hat_bias_error_level = NaN(N_sim,length(mesurement_error_level)); 
-
-% 4.5. Nested for loops for simulation and measurement error level
-for j = 1:length(mesurement_error_level)
+% 4.3. Nested for loops for simulation and measurement error level
+for j = 1:length(measurement_error_level)
     X_with_measurement_error = X+random('Normal',0,1,[N_obs 1]) ...
-        *sqrt(mesurement_error_level(j)); 
+        *sqrt(measurement_error_level(j)); 
     for i = 1:N_sim
         u = random('Normal',0,1,[N_obs 1]);
         y = X*B_true+u;
         LSS = exercisefunctionlss(y,X_with_measurement_error); 
         B_hat(i,1) = LSS.B_hat(1,1);
-        B_hat_bias(i) = abs(LSS.B_hat(1,1)-B_true);
     end
-    B_hat_error_level(:,:,j) = B_hat;
-    B_hat_bias_error_level(:,j) = B_hat_bias;
+    B_hat_measurement_error_level(:,:,j) = B_hat;
 end
 
 %% 5. Plot the sampling distribution of the OLS estimator
 
 % 5.1. Kernel density estimation
-[f1,x1] = ksdensity(B_hat_error_level(:,1,1));
-[f2,x2] = ksdensity(B_hat_error_level(:,1,6));
-[f3,x3] = ksdensity(B_hat_error_level(:,1,11));
+[f1,x1] = ksdensity(B_hat_measurement_error_level(:,1,1));
+[f2,x2] = ksdensity(B_hat_measurement_error_level(:,1,6));
+[f3,x3] = ksdensity(B_hat_measurement_error_level(:,1,11));
 
-% 5.2. Sampling distribution of OLS estimator subject to mes. err.
+% 5.2. Sampling distribution of OLS estimator subject to measurement error
 figure
 hold on
-plot(x1,f1,'DisplayName','\sigma = 0');
-plot(x2,f2,'DisplayName','\sigma = 0.5');
-plot(x3,f3,'DisplayName','\sigma = 1');
+plot(x1,f1,'DisplayName','Measurement error variance = 0');
+plot(x2,f2,'DisplayName','Measurement error variance = 0.5');
+plot(x3,f3,'DisplayName','Measurement error variance = 1');
 xlabel('B\_hat');
 ylabel('Density');
-title(['Fig. 1. Measurement error and ' ...
+title(['Fig. 1. Effect of measurement error on ' ...
     'the sampling distribution of OLS estimator']);
 legend('show');
 hold off
