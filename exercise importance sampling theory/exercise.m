1e7baec504daaee85e62a1519432e5aaf65aecff
Tunga Kantarcı <44529911+tungakantarci@users.noreply.github.com>
2025-11-11 00:14:54 +0100

    Add files via upload

---
+++ exercise importance sampling theory/exercise.m
@@ -0,0 +1,34 @@
+% Exercise - Understanding the theory of importance sampling
+
+%% 1. Aim of the exercise
+% Text.
+
+%% 2. Tails comparison
+
+% 2.1. Sequence from -5 to 5 by steps of 0.1
+x_values = -5:0.1:5; 
+
+% 2.2. Theoretical Standard Normal PDF
+PD_Normal = makedist('Normal','mu',0,'sigma',1);
+
+% 2.3. Calculate the PDF for the Normal distribution
+PDF_Normal = pdf(PD_Normal,x_values);
+
+% 2.4. Theoretical standard cauchy PDF
+PD_Cauchy = makedist('tLocationScale','mu',0,'sigma',1,'nu',1);
+
+% 2.5. Title
+PDF_Cauchy = pdf(PD_Cauchy,x_values);
+
+%% Plot the theoretical PDFs
+
+% Plot
+figure
+hold on
+plot(x_values,PDF_Normal,'DisplayName','N(0,1)')
+plot(x_values,PDF_Cauchy,'DisplayName','Cauchy(0,1)')
+title('Fig. 1. Title')
+xlabel('x')
+ylabel('PDF')
+legend('show');
+hold off
